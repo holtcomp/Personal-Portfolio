76bfb33e3a088bb3268ccb243ba453ec07b655de
holtcomp <mark@holtcomputers.co.uk>
2022-12-14 14:45:19 +0000

    commit 4

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,15 +1,15 @@
+     console.log("hello")
+    // const timeContainer = document.getElementById("time-container");
+    // const BIRTH_DAY = "09-19-1963";
+    // const BIRTH_DAY_DATE = new Date(BIRTH_DAY);
+    // const intlNumberFormatter = new Intl.NumberFormat("en-US");
 
-    const timeContainer = document.getElementById("time-container");
-    const BIRTH_DAY = "09-19-1963";
-    const BIRTH_DAY_DATE = new Date(BIRTH_DAY);
-    const intlNumberFormatter = new Intl.NumberFormat("en-US");
+    // function setInterval() {
+    //   const now = new Date();
+    //   const difference = Math.floor(
+    //     (now.getTime() - BIRTH_DAY_DATE.getTime()) / 1000
+    //   );
 
-    function setInterval() {
-      const now = new Date();
-      const difference = Math.floor(
-        (now.getTime() - BIRTH_DAY_DATE.getTime()) / 1000
-      );
-
-      timeContainer.innerText = intlNumberFormatter.format(difference);
-    };
+    //   timeContainer.innerText = intlNumberFormatter.format(difference);
+    // };
   
